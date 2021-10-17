c4f698a96369d6b9f6708070cfffff08f3811783
= <=>
2021-10-17 17:08:41 +0200

    add controll callback

--- Camera.cpp
+++ Camera.cpp
@@ -1,11 +1,43 @@
 #include "Camera.hpp"
 
-void Camera::look() {
-  
+#include "Scene.hpp"
+
+void Camera::adjustTarget(double mouseXPos, double mouseYPos) {
+  // printf("x=[%f], y=[%f] \n", mouseXPos, mouseYPos);
+
+  float xoffset = mouseXPos - lastX;
+  float yoffset =
+      lastY - mouseYPos;  // reversed since y-coordinates range from bottom to top
+  lastX = mouseXPos;
+  lastY = mouseYPos;
+  yaw
+
+  // printf("%f\n", r);
+
+  // float fi = mouseXPos * 0.5 + mouseYPos;
+  // float psi = mouseYPos * 0.5 + mouseXPos;
+  // target.x = cos(fi);
+  // target.z = sin(fi);
+  // target.y = sin(psi);
+
+  scene->updateCamera();
 }
 
-glm::mat4 Camera::getCamera() { return glm::lookAt(eye, eye + target, up); }
+glm::mat4 Camera::getCamera() {
+  return glm::lookAt(
+      glm::vec3(10, 10, 10),  // Camera is at (4,3,-3), in World Space
+      glm::vec3(0, 0, 0),     // and looks at the origin
+      glm::vec3(0, 1, 0)      // Head is up (set to 0,-1,0 to look upside-down)
+  );
+  // glm::lookAt(eye, eye + target, up);
+}
 
-Camera::Camera() {}
+Camera::Camera(Scene *scene) {
+  shader = new Shader();
+  this->scene = scene;
+  projectionMatrix =
+      glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 0.1f, 100.0f);
+  viewMatrix = getCamera();
+}
 
 Camera::~Camera() {}
